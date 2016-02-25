21eaaa7f57550677a4b7f5e110f1b1a56825347d
Max Bauer <mbauer7532@gmail.com>
2016-02-25 20:40:01 +0200

    Creation.

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Neo on 2/25/2016.
+ */
+public final class Main {
+
+    public static void main(String[] argv) {
+        System.out.println("Hello world.");
+    }
+}
