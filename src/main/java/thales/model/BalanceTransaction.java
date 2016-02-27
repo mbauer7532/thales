75e01bb266819f48acd655a6d568bf7ad9e67dc5
Max Bauer <mbauer7532@gmail.com>
2016-02-28 01:22:15 +0200

    Balance transactions.

--- src/main/java/thales/model/BalanceTransaction.java
+++ src/main/java/thales/model/BalanceTransaction.java
@@ -1,7 +1,55 @@
 package thales.model;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by Neo on 2/26/2016.
  */
 public final class BalanceTransaction {
+
+    public static BalanceTransaction create(final long transactionId, final long login, final TransactionType transactionType, final double amount, final LocalDateTime depositDateTime, final String comment) {
+
+        return new BalanceTransaction(transactionId, login, transactionType, amount, depositDateTime, comment);
+    }
+
+    public long getTransactionId() {
+        return mTransactionId;
+    }
+
+    public long getLogin() {
+        return mLogin;
+    }
+
+    public TransactionType getTransactionType() {
+        return mTransactionType;
+    }
+
+    public double getAmount() {
+        return mAmount;
+    }
+
+    public LocalDateTime getDepositDateTime() {
+        return mDepositDateTime;
+    }
+
+    public String getComment() {
+        return mComment;
+    }
+
+    private BalanceTransaction(final long transactionId, final long login, final TransactionType transactionType, final double amount, final LocalDateTime depositDateTime, final String comment) {
+
+        mTransactionId = transactionId;
+        mLogin = login;
+        mTransactionType = transactionType;
+        mAmount = amount;
+        mDepositDateTime = depositDateTime;
+        mComment = comment;
+    }
+
+    private final long mTransactionId;
+    private final long mLogin;
+    private final TransactionType mTransactionType;
+    private final double mAmount;
+    private final LocalDateTime mDepositDateTime;
+    private final String mComment;
 }
