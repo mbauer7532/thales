aaff5b4d75421f28b55a2c3c13ff47364aaf648a
Max Bauer <mbauer7532@gmail.com>
2016-02-28 00:59:03 +0200

    Work continues.

--- src/main/java/thales/model/ClosedTrade.java
+++ src/main/java/thales/model/ClosedTrade.java
@@ -1,7 +1,105 @@
 package thales.model;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
 /**
  * Created by Neo on 2/26/2016.
  */
 public final class ClosedTrade {
+
+    public static ClosedTrade create(
+        final long tradeId,
+        final long login,
+        final String symbol,
+        final TradeType tradeType,
+        final int volume,
+        final LocalDateTime openDateTime,
+        final double openPrice,
+        final LocalDateTime closeDateTime,
+        final double closePrice,
+        final OptionalDouble stopLoss,
+        final OptionalDouble takeProfit,
+        final Optional<LocalDateTime> pendingOrderExpiration,
+        final double convRateOpen,
+        final double convRateClosed,
+        final double commission,
+        final double swaps,
+        final double profit,
+        final double taxes,
+        final double marginRate,
+        final LocalDateTime timestamp,
+        final String comment) {
+
+        return new ClosedTrade(tradeId, login, symbol, tradeType, volume, openDateTime, openPrice, closeDateTime, closePrice, stopLoss, takeProfit, pendingOrderExpiration, convRateOpen, convRateClosed, commission, swaps, profit, taxes, marginRate, timestamp, comment);
+    }
+
+    private ClosedTrade(
+        final long tradeId,
+        final long login,
+        final String symbol,
+        final TradeType tradeType,
+        final int volume,
+        final LocalDateTime openDateTime,
+        final double openPrice,
+        final LocalDateTime closeDateTime,
+        final double closePrice,
+        final OptionalDouble stopLoss,
+        final OptionalDouble takeProfit,
+        final Optional<LocalDateTime> pendingOrderExpiration,
+        final double convRateOpen,
+        final double convRateClosed,
+        final double commission,
+        final double swaps,
+        final double profit,
+        final double taxes,
+        final double marginRate,
+        final LocalDateTime timestamp,
+        final String comment) {
+
+        mTradeId = tradeId;
+        mLogin = login;
+        mSymbol = symbol;
+        mTradeType = tradeType;
+        mVolume = volume;
+        mOpenDateTime = openDateTime;
+        mOpenPrice = openPrice;
+        mCloseDateTime = closeDateTime;
+        mClosePrice = closePrice;
+        mStopLoss = stopLoss;
+        mTakeProfit = takeProfit;
+        mPendingOrderExpiration = pendingOrderExpiration;
+        mConvRateOpen = convRateOpen;
+        mConvRateClosed = convRateClosed;
+        mCommission = commission;
+        mSwaps = swaps;
+        mProfit = profit;
+        mTaxes = taxes;
+        mMarginRate = marginRate;
+        mTimestamp = timestamp;
+        mComment = comment;
+    }
+
+    private final long mTradeId;
+    private final long mLogin;
+    private final String mSymbol;
+    private final TradeType mTradeType;
+    private final int mVolume;
+    private final LocalDateTime mOpenDateTime;
+    private final double mOpenPrice;
+    private final LocalDateTime mCloseDateTime;
+    private final double mClosePrice;
+    private final OptionalDouble mStopLoss;
+    private final OptionalDouble mTakeProfit;
+    private final Optional<LocalDateTime> mPendingOrderExpiration;
+    private final double mConvRateOpen;   // convertation rates from profit currency to group deposit currency
+    private final double mConvRateClosed; // convertation rates from profit currency to group deposit currency
+    private final double mCommission;
+    private final double mSwaps;
+    private final double mProfit;
+    private final double mTaxes;
+    private final double mMarginRate;       // margin convertation rate (rate of convertation from margin currency to deposit one)
+    private final LocalDateTime mTimestamp; // timestamp
+    private final String mComment;
 }
--- src/main/java/thales/model/Encoders.java
+++ src/main/java/thales/model/Encoders.java
@@ -1,13 +1,13 @@
 package thales.model;
 
 import thales.model.protoBuf.LocalDateTimeProto;
-import thales.model.protoBuf.OpenTradeProto;
-import thales.model.protoBuf.OpenTradeProto.Builder;
-import thales.model.protoBuf.UserProto;
-import thales.utils.Functionals;
+import thales.model.protoBuf.OpenTradeResponseProto;
+import thales.model.protoBuf.UserResponseProto;
 
 import java.time.LocalDateTime;
-import java.util.function.Consumer;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalLong;
 
 /**
  * Created by Neo on 2/26/2016.
@@ -15,29 +15,81 @@ import java.util.function.Consumer;
 
 public final class Encoders {
 
-    public static byte[] encode(final User user) {
+    public static byte[] encodeUserRequest(
+        final OptionalLong userId,
+        final String firstName,
+        final String lastName,
+        final boolean isEnabled,
+        final boolean isReadOnly,
+        final boolean sendReports,
+        final int leverage,
+        final String email,
+        final String comment) {
 
-        return UserProto.newBuilder()
-                .setUserId(user.getUserId())
-                .setFirstName(user.getFirstName())
-                .setLastName(user.getLastName())
-                .setIsEnabled(user.getIsEnabled())
-                .setIsReadOnly(user.getIsReadOnly())
-                .setSendReports(user.getSendReports())
-                .setLeverage(user.getLeverage())
-                .setEmail(user.getEmail())
-                .setComment(user.getComment())
-                .setBalance(user.getBalance())
-                .setCredit(user.getCredit())
-                .setTaxes(user.getTaxes())
-                .build()
-                .toByteArray();
+        final UserResponseProto.Builder builder = UserResponseProto.newBuilder();
+
+        userId.ifPresent(builder::setUserId);
+
+        return builder.setFirstName(firstName)
+                      .setLastName(lastName)
+                      .setIsEnabled(isEnabled)
+                      .setIsReadOnly(isReadOnly)
+                      .setSendReports(sendReports)
+                      .setLeverage(leverage)
+                      .setEmail(email)
+                      .setComment(comment)
+                      .build()
+                      .toByteArray();
     }
 
-    public static byte[] encode(final OpenTrade openTrade) {
+    public static byte[] encodeUserResponse(final User user) {
 
-        final Builder builder =
-            OpenTradeProto.newBuilder()
+        return UserResponseProto.newBuilder()
+            .setUserId(user.getUserId())
+            .setFirstName(user.getFirstName())
+            .setLastName(user.getLastName())
+            .setIsEnabled(user.getIsEnabled())
+            .setIsReadOnly(user.getIsReadOnly())
+            .setSendReports(user.getSendReports())
+            .setLeverage(user.getLeverage())
+            .setEmail(user.getEmail())
+            .setComment(user.getComment())
+            .setBalance(user.getBalance())
+            .setCredit(user.getCredit())
+            .setTaxes(user.getTaxes())
+            .build()
+            .toByteArray();
+    }
+
+    public static byte[] encodeOpenTradeRequest(
+        final long login,
+        final String symbol,
+        final TradeType tradeType,
+        final int volume,
+        final OptionalDouble stopLoss,
+        final OptionalDouble takeProfit,
+        final Optional<LocalDateTime> pendingTradeExpiration,
+        final String comment) {
+
+        final OpenTradeResponseProto.Builder builder =
+            OpenTradeResponseProto.newBuilder()
+                .setLogin(login)
+                .setSymbol(symbol)
+                .setTradeType(tradeType.toCmd())
+                .setVolume(volume)
+                .setComment(comment);
+
+        stopLoss.ifPresent(builder::setStopLoss);
+        takeProfit.ifPresent(builder::setTakeProfit);
+        pendingTradeExpiration.ifPresent(expiration -> builder.setPendingTradeExpiration(toProtoObject(expiration)));
+
+        return builder.build().toByteArray();
+    }
+
+    public static byte[] encodeOpenTradeResponse(final OpenTrade openTrade) {
+
+        final OpenTradeResponseProto.Builder builder =
+            OpenTradeResponseProto.newBuilder()
                 .setTradeId(openTrade.getTradeId())
                 .setLogin(openTrade.getLogin())
                 .setSymbol(openTrade.getSymbol())
@@ -50,17 +102,16 @@ public final class Encoders {
         openTrade.getTakeProfit().ifPresent(builder::setTakeProfit);
         openTrade.getPendingOrderExpiration().ifPresent(expiration -> builder.setPendingTradeExpiration(toProtoObject(expiration)));
 
-        return
-            builder.setConvRateOpen(openTrade.getConvRateOpen())
-                   .setCommission(openTrade.getCommission())
-                   .setSwaps(openTrade.getSwaps())
-                   .setProfit(openTrade.getProfit())
-                   .setTaxes(openTrade.getTaxes())
-                   .setMarginRate(openTrade.getMarginRate())
-                   .setTimestamp(toProtoObject(openTrade.getTimestamp()))
-                   .setComment(openTrade.getComment())
-                   .build()
-                   .toByteArray();
+        return builder.setConvRateOpen(openTrade.getConvRateOpen())
+            .setCommission(openTrade.getCommission())
+            .setSwaps(openTrade.getSwaps())
+            .setProfit(openTrade.getProfit())
+            .setTaxes(openTrade.getTaxes())
+            .setMarginRate(openTrade.getMarginRate())
+            .setTimestamp(toProtoObject(openTrade.getTimestamp()))
+            .setComment(openTrade.getComment())
+            .build()
+            .toByteArray();
     }
 
     private static LocalDateTimeProto toProtoObject(final LocalDateTime ldt) {
